750801e66b2bb8b12195116c2ac1c7d66775084a
Daniil Pankov <dpancov64@gmail.com>
2025-02-24 23:35:44 +0300

    base-path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  basePath: "/mene",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/mene/" : undefined,
 };
 
 export default nextConfig;
